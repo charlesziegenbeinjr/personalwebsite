01789e74b2991bb6c6d41fada7758066039888b9
Charlie Ziegenbein <46666647+charlieziegenbein@users.noreply.github.com>
2019-11-23 13:09:38 -0600

    Changes to carousel, js added

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// This will contain JS functionality
+function pop_up(i) {
+	if (i == 1) {
+		alert("To learn more about my experience at Washington University in St. Louis as both an undergraduate student and researcher, please go to the 'Research and Experience' Page");
+	}
+	if (i == 2) {
+		alert("To learn more about my experience at the Sloan Water Company as a Software Engineering Intern, please go to the 'Research and Experience' Page");
+	}
+	if (i == 3) {
+		alert("To learn more about my experience at OSRAM GmbH as a Competitor Surveillance Intern , please go to the 'Research and Experience' Page");
+	}
+}
