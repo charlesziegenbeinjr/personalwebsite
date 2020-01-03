94ff4dee4c57dad750fbde1d88f0bc57f79df7f1
Charlie Ziegenbein <46666647+charlieziegenbein@users.noreply.github.com>
2020-01-02 23:56:53 -0500

    Changes 1.2.2020

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,1 +1,19 @@
+var news_api = "e0d63ae57c754738b29f29d664cec383";
 
+var get_news = new XMLHttpRequest();
+get_news.onreadystatechange = function() {
+
+if (this.readyState == 4 && this.status == 200) {
+    var newVar = JSON.parse(this.responseText);
+    console.log(newVar);
+
+    for (var i = 0; i < 10 ; ++i) {
+      render(newVar.articles[i]);
+    }
+
+  } else if (this.readyState == 4) {
+    console.log(this.responseText);
+  }
+}
+
+get_news.open("GET", "")
