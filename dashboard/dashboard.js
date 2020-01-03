a5989f729e2ec2dd32de337dfffd0bc043f596d6
Charlie Ziegenbein <46666647+charlieziegenbein@users.noreply.github.com>
2020-01-03 02:51:08 -0500

    Changes 1.3.2020

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -16,4 +16,25 @@ if (this.readyState == 4 && this.status == 200) {
   }
 }
 
-get_news.open("GET", "")
+get_news.open("GET", "https://newsapi.org/v2/top-headlines?country=us&apiKey=e0d63ae57c754738b29f29d664cec383", true);
+get_news.setRequestHeader("x-api-key", news_api);
+get_news.send();
+
+function render(items) {
+	var article = document.createElement("li");
+	var articletitle = document.createElement("p");
+	articletitle.innerHTML = items.title;
+	var articlelink = document.createElement("a");
+	articlelink.href = items.url;
+	var space1 = document.createElement("p");
+	space1.innerHTML = " ~ ";
+	articlelink.innerHTML = "Link To Article Here";
+	var space2 = document.createElement("p");
+	space2.innerHTML = " ~ ";
+	article.appendChild(articletitle);
+	article.appendChild(space1);
+	article.appendChild(articlelink);
+	article.appendChild(space2);
+	article.classList.add("listitems");
+	document.getElementById("newslist").appendChild(article);
+}
